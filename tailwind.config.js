6f630eca70e940c9a706090c9b1c7e6720276c9c
latz09 <85132328+latz09@users.noreply.github.com>
2022-03-28 17:15:38 -0500

    restructured

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     './layouts/*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-darkest': '#05386b',
+        'primary-dark': '#379683',
+        'primary': '#5CDB95',
+        'primary-light': '#8EE4AF',
+        'primary-lightest': 'EDF5E1'
+
+      }
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
